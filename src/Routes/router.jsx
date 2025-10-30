d0af52c6831d39722c550b7925fcee834414766e
Diptajett Roy <diptajeet345@gmail.com>
2025-10-30 10:44:19 +0600

    first commit

---
+++ src/Routes/router.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router';
+import Root from '../Page/Root';
+
+const router=createBrowserRouter([
+    {
+        path:"/",
+        element:<Root></Root>
+    }
+])
+
+export default router;
